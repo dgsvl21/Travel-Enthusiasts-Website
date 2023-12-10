e6dbeb028ea983492e9df4bdaf0d488f7ab48e2e
Diogo Silva <69628907+DgSvlFx@users.noreply.github.com>
2023-12-10 20:40:55 +0000

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -6,3 +6,19 @@ function scrollToMain() {
   })
 }
 
+const destinations = {
+  //adicionar destinos
+  //exemplo -> porto: 'porto.html'
+};
+
+function searchDestination() {
+  var destination = document.getElementById("searchInput").value.toLowerCase();
+  console.log("Destination:", destination);
+
+  if (destinations.hasOwnProperty(destination)) {
+    var destinationUrl = destinations[destination];
+    window.location.href = destinationUrl;
+  } else {
+  alert('Not found');
+  }
+}
