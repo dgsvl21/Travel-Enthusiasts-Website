0011bf0f3b68bd57fc4b303f492cc4d76340e04a
Diogo Silva <69628907+DgSvlFx@users.noreply.github.com>
2024-01-21 18:37:26 +0000

    Add files via upload

---
+++ Porto Page/portoscripts.js
@@ -0,0 +1,21 @@
+//arrow down animation btn
+function scrollToMain() {
+const mainSection = document.getElementById("main");
+window.scrollTo({
+    top: mainSection.offsetTop,
+    behavior: "smooth"
+})
+}
+
+//scroll to-top btn
+function scrollToTop() {
+window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+})
+}
+  
+
+  
+  
+  
