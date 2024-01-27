0e665d2db1607f3f5280d666e7ae55c3c9f5d2a2
Diogo Silva <69628907+DgSvlFx@users.noreply.github.com>
2024-01-27 23:58:29 +0000

    Add files via upload

--- Porto Page/portoscripts.js
+++ Porto Page/portoscripts.js
@@ -14,7 +14,49 @@ window.scrollTo({
     behavior: "smooth"
 })
 }
-  
+
+//PoI btn scroll
+const PoIbtn = document.getElementById('PoIbtn');
+
+PoIbtn.addEventListener('click', function() {
+  const PoITitleElement = document.querySelector('.PoI-title');
+
+  if (PoITitleElement) {
+    const offsetTop = PoITitleElement.offsetTop;
+
+    window.scrollTo({
+      top: offsetTop,
+      behavior: 'smooth'
+    });
+  }
+});
+
+
+//PoI Image Slider
+const slider = document.querySelector('.slider');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+let currentIndex = 0;
+
+function changeSlide(direction) {
+    if (slider.children.length > 0) {
+    currentIndex += direction;
+
+    if (currentIndex < 0) {
+        currentIndex = slider.children.length - 1;
+    } else if (currentIndex >= slider.children.length) {
+        currentIndex = 0;
+    }
+
+    const translateValue = -currentIndex * 100;
+    slider.style.transform = `translateX(${translateValue}%)`;
+    }
+}
+
+prevBtn.addEventListener('click', () => changeSlide(-1));
+nextBtn.addEventListener('click', () => changeSlide(1));
+
 
   
   
